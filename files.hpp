79425a8b9e2f054d9b0ec4ec677e80fa1f33cd19
abdelkarim <kimoacm@gmail.com>
2019-08-21 14:01:19 +0200

    add windows suport

--- files.hpp
+++ files.hpp
@@ -1,20 +1,12 @@
 #include <string>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-
-
-#ifndef HELLO_HPP_INCLUDED
-#define HELLO_HPP_INCLUDED
+#ifndef FILLES_HPP_INCLUDED
+#define FILLES_HPP_INCLUDED
 
 namespace files {
   void write_to_file(std::string path, std::string text);
   void cpy(std::string path);
   std::string get_dir(std::string path);
   std::string get_file_name(std::string path);
-
 }
 #endif
